9e57a07bf5ac1a6661651b178a420f8e476fc3fd
sabler <gizmo.00bases@icloud.com>
2023-06-17 21:09:17 -0700

    Next section is dynamic routes

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Lorem ipsum dolor sit amet]</p>
+        <p>Lorem ipsum dolor sit amet</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href='https://nextjs.org/learn'>our Next.js tutorial</a>.)
